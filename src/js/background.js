9fceea3c77604ff7e11a5124fb933edc3795a741
Benjamen Brescka <benjamen.brescka@gmail.com>
2019-02-11 03:40:02 -0600

    Added standard JS to dev dependencies and made changes to conform to the standard.

--- src/js/background.js
+++ src/js/background.js
@@ -1,37 +1,37 @@
 const topLevelMenu = chrome.contextMenus.create({
-  "id": "topLevel",
-  "title": "flipper",
-  "contexts": ["all"]
+  'id': 'topLevel',
+  'title': 'flipper',
+  'contexts': ['all']
 })
 
-const flipHorizontallyMenu = chrome.contextMenus.create({
-  "id": "horizontalFlip",
-  "title": "Flip Horizontally (Mirror)",
-  "parentId" : topLevelMenu,
-  "contexts": ["all"]
+chrome.contextMenus.create({
+  'id': 'horizontalFlip',
+  'title': 'Flip Horizontally (Mirror)',
+  'parentId': topLevelMenu,
+  'contexts': ['all']
 })
 
-const flipVerticallyMenu = chrome.contextMenus.create({
-  "id": "verticalFlip",
-  "title": "Flip Vertically",
-  "parentId" : topLevelMenu,
-  "contexts": ["all"]
+chrome.contextMenus.create({
+  'id': 'verticalFlip',
+  'title': 'Flip Vertically',
+  'parentId': topLevelMenu,
+  'contexts': ['all']
 })
 
-const rotateRightMenu = chrome.contextMenus.create({
-  "id": "rotateRight",
-  "title": "Rotate Right (90° Clockwise)",
-  "parentId" : topLevelMenu,
-  "contexts": ["all"]
+chrome.contextMenus.create({
+  'id': 'rotateRight',
+  'title': 'Rotate Right (90° Clockwise)',
+  'parentId': topLevelMenu,
+  'contexts': ['all']
 })
 
-const rotateLeftMenu = chrome.contextMenus.create({
-  "id": "rotateLeft",
-  "title": "Rotate Left (90° Counterclockwise)",
-  "parentId" : topLevelMenu,
-  "contexts": ["all"]
+chrome.contextMenus.create({
+  'id': 'rotateLeft',
+  'title': 'Rotate Left (90° Counterclockwise)',
+  'parentId': topLevelMenu,
+  'contexts': ['all']
 })
 
-chrome.contextMenus.onClicked.addListener((data, tab)=>{
-  chrome.tabs.sendMessage(tab.id, data.menuItemId);
-});
+chrome.contextMenus.onClicked.addListener((data, tab) => {
+  chrome.tabs.sendMessage(tab.id, data.menuItemId)
+})
--- src/js/content.js
+++ src/js/content.js
@@ -1,63 +1,63 @@
-// Injected JS is wrapped in an IIFE to eliminate 
+// Injected JS is wrapped in an IIFE to eliminate
 // possiblity of contaminating any scope.
 (() => {
   // This variable holds the last element that the user
   // triggered the context menu on.
-  let targetElement;
-  
+  let targetElement
+
   // Data structure that allows us to keep track of different
   // elements's own rotation angles in an easy way.
-  let angleMap = new Map([]);
+  let angleMap = new Map([])
 
   const contextMenuHandler = (event) => {
-    targetElement = event.target;
-  };
+    targetElement = event.target
+  }
 
   const rotate = (element, rotationAngle) => {
-    normalizeElement(element);
-    let currentAngle;
+    normalizeElement(element)
+    let currentAngle
     if (!angleMap.has(element)) {
-      angleMap.set(element, 0);
-      currentAngle = 0;
+      angleMap.set(element, 0)
+      currentAngle = 0
     } else {
-      currentAngle = angleMap.get(element);
+      currentAngle = angleMap.get(element)
     }
-    const newAngle = currentAngle + rotationAngle;
-    element.setAttribute("style", `transform: rotate(${newAngle}deg)`);
-    angleMap.set(element, newAngle);
-  };
+    const newAngle = currentAngle + rotationAngle
+    element.setAttribute('style', `transform: rotate(${newAngle}deg)`)
+    angleMap.set(element, newAngle)
+  }
 
   const normalizeElement = (element) => {
-    if (getComputedStyle(element, null).display === "inline") {
-      element.classList.add("flipperNormalized");
+    if (window.getComputedStyle(element, null).display === 'inline') {
+      element.classList.add('flipperNormalized')
     }
   }
 
   const flipHorizontally = (element) => {
-    normalizeElement(element);
-    element.classList.toggle("horizontalFlip");
+    normalizeElement(element)
+    element.classList.toggle('horizontalFlip')
   }
 
   const flipVertically = (element) => {
-    normalizeElement(element);
-    element.classList.toggle("verticalFlip");
+    normalizeElement(element)
+    element.classList.toggle('verticalFlip')
   }
 
-  document.addEventListener("contextmenu", contextMenuHandler);
+  document.addEventListener('contextmenu', contextMenuHandler)
   chrome.runtime.onMessage.addListener((message) => {
-    switch(message) {
-      case "horizontalFlip":
-        flipHorizontally(targetElement);
-        break;
-      case "verticalFlip":
-        flipVertically(targetElement);
-        break;
-      case "rotateRight":
-        rotate(targetElement, 90);
-        break;
-      case "rotateLeft":
-        rotate(targetElement, -90);
-        break;
+    switch (message) {
+      case 'horizontalFlip':
+        flipHorizontally(targetElement)
+        break
+      case 'verticalFlip':
+        flipVertically(targetElement)
+        break
+      case 'rotateRight':
+        rotate(targetElement, 90)
+        break
+      case 'rotateLeft':
+        rotate(targetElement, -90)
+        break
     }
-  });
+  })
 })()
